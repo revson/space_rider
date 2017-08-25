d7829e71c6adda637db2ab02d42bd401a0b089f8
revson <revson@gmail.com>
2017-08-25 14:09:37 -0300

    add files;

---
+++ Assets/Script/moveOffset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOffset : MonoBehaviour {
+
+	private Material currentMaterial;
+	public float speed;
+	private float offset;
+	// Use this for initialization
+	void Start () {
+		currentMaterial = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		offset += speed * 0.001f;
+		currentMaterial.SetTextureOffset ("_MainTex", new Vector2(0, offset));
+	}
+}
